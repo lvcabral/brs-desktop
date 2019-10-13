806a473f150530f38fdda0fe885606e75ff2a52c
Marcelo Lv Cabral <lvcabral@hotmail.com>
2019-10-13 16:28:55 -0700

    Implemented support for SD and FHS and TV Overscan #6 #13

--- src/menu/deviceMenuTemplate.js
+++ src/menu/deviceMenuTemplate.js
@@ -5,32 +5,63 @@ export const deviceMenuTemplate = {
     submenu: [
         {
             id: "device-480p",
-            label: "SD 480p (Tyler)",
+            label: "Display Mode: SD 480p (4:3)",
             type: "radio",
             checked: false,
             click: () => {
                 var window = BrowserWindow.getFocusedWindow();
-                window.webContents.send("setDevice", "480p");
+                window.webContents.send("setDisplay", "480p");
             }
         },
         {
             id: "device-720p",
-            label: "HD 720p (Midland)",
+            label: "Display Mode: HD 720p (16:9)",
             type: "radio",
             checked: true,
             click: () => {
                 var window = BrowserWindow.getFocusedWindow();
-                window.webContents.send("setDevice", "720p");
+                window.webContents.send("setDisplay", "720p");
             }
         },
         {
             id: "device-1080p",
-            label: "FHD 1080p (Cooper)",
+            label: "Display Mode: FHD 1080p (16:9)",
             type: "radio",
             checked: false,
             click: () => {
                 var window = BrowserWindow.getFocusedWindow();
-                window.webContents.send("setDevice", "1080p");
+                window.webContents.send("setDisplay", "1080p");
+            }
+        },
+        { type: "separator" },
+        {
+            id: "overscan-disabled",
+            label: "TV Overscan: Disabled",
+            type: "radio",
+            checked: true,
+            click: () => {
+                var window = BrowserWindow.getFocusedWindow();
+                window.webContents.send("setOverscan", "disabled");
+            }
+        },
+        {
+            id: "overscan-guide-lines",
+            label: "TV Overscan: Guide Lines",
+            type: "radio",
+            checked: false,
+            click: () => {
+                var window = BrowserWindow.getFocusedWindow();
+                window.webContents.send("setOverscan", "guide-lines");
+            }
+        },
+        {
+            id: "overscan-enabled",
+            label: "TV Overscan: Enabled",
+            type: "radio",
+            checked: false,
+            click: () => {
+                var window = BrowserWindow.getFocusedWindow();
+                window.webContents.send("setOverscan", "enabled");
             }
         },
         { type: "separator" },
